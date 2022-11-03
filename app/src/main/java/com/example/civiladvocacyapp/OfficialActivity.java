f8ddd8e7abef2c9a8ca130ee0f49f6cf6903caea
David Galicia <dgalicia@fourier.cs.iit.edu>
2022-11-02 19:49:25 -0500

    added in new activity

---
+++ app/src/main/java/com/example/civiladvocacyapp/OfficialActivity.java
@@ -0,0 +1,30 @@
+package com.example.civiladvocacyapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OfficialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_official);
+        setTitle("Civil Advocacy");
+
+    }
+
+    public void clickYT(View v){
+
+    }
+
+    public void clickTwitter(View v){
+
+    }
+
+    public void clickFb(View v){
+
+    }
+}
